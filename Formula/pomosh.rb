dd5fa046bf995af5cbbe9bef2a3245fc47269351
rolemadelen <rolemadelen17@pm.me>
2024-03-21 18:16:07 -0400

    fix: sha256

--- Formula/pomosh.rb
+++ Formula/pomosh.rb
@@ -2,10 +2,10 @@
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Pomosh < Formula
-    desc "Command-line pomodoro timer written in Rust"
+    desc "Command-line Pomodoro Timer written in Rust"
     homepage "https://github.com/rolemadelen/pomosh"
-    url "https://github.com/rolemadelen/pomosh/archive/refs/tags/v0.1.0.tar.gz"
-    sha256 "da8635e0092f6c1b6e9f90bd037c27e917cfa9cf71a2d5167ddf428311257953"
+    url "https://github.com/rolemadelen/pomosh/releases/download/v0.1.0/pomosh-mac.tar.gz"
+    sha256 "f7359406f066485e4ecdde1216c63ca1e8c21eb8a4735c3aa3ceaf808b9bdd90"
     version "0.1.0"
   
     def install
