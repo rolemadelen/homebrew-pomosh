7d959828bd2b4c4dc1718582260da0c1669a0d56
rolemadelen <rolemadelen17@pm.me>
2024-03-27 21:08:46 -0400

    update: v0.1.4

--- Formula/pomosh.rb
+++ Formula/pomosh.rb
@@ -4,9 +4,9 @@
 class Pomosh < Formula
     desc "Command-line Pomodoro Timer written in Rust"
     homepage "https://github.com/rolemadelen/pomosh"
-    url "https://github.com/rolemadelen/pomosh/releases/download/v0.1.3/pomosh-mac.tar.gz"
-    sha256 "96b2064d7eddd9c174efa72a5da656ff671316bd48adfd6522df8a0fae7cfea9"
-    version "0.1.3"
+    url "https://github.com/rolemadelen/pomosh/releases/download/v0.1.4/pomosh-mac.tar.gz"
+    sha256 "d3dab0201d185573e4bf337a571b9f63dfba9e9be28901987faed05a3467bce8"
+    version "0.1.4"
   
     def install
       bin.install "pomosh"
