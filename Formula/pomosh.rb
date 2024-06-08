772b000e6ab8114ac9d737f4c3ab02628ae5f9c7
rolemadelen <rolemadelen17@pm.me>
2024-06-08 09:59:54 -0400

    Update pomosh.rb

--- Formula/pomosh.rb
+++ Formula/pomosh.rb
@@ -3,8 +3,8 @@
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Pomosh < Formula
     desc "Command-line Pomodoro Timer written in Rust"
-    homepage "https://github.com/img9417/pomosh"
-    url "https://github.com/img9417/pomosh/releases/download/v0.1.5/pomosh-mac.tar.gz"
+    homepage "https://github.com/rolemadelen/pomosh"
+    url "https://github.com/rolemadelen/pomosh/releases/download/v0.1.5/pomosh-mac.tar.gz"
     sha256 "79362d2451f86d46eebc295426a1950c969bb66d570b4f172e6cf715894d0166"
     version "0.1.5"
   
